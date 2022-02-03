58801fcf12640af9305776f60871e161487626c9
Joao Artur <joaoartur4590@gmail.com>
2022-02-02 22:05:46 -0300

    up1

---
+++ scripts/sript.js
@@ -0,0 +1,15 @@
+let front = "front_card";
+let back = "back_card";
+
+let cards = [
+  "react",
+  "node",
+  "mongo",
+  "jquery",
+  "javascript",
+  "html",
+  "css",
+  "firebase",
+  "electron",
+  "bootstrap",
+];
